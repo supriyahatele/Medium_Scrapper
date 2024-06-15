140b703ecd9091b6818d94b2f6188053194202f4
supriyahatele <niyahatele70@gmail.com>
2024-06-15 20:54:09 +0530

    : update done in route file

---
+++ server/controllers/articleScrapperController.js
@@ -0,0 +1,30 @@
+const Article = require("../models/articleModel");
+
+const createScrapArticle=  async (req, res) => {
+    const { topic } = req.query;
+  
+    if (!topic) {
+      return res.status(400).json({ error: 'Topic is required' });
+    }
+  
+    try {
+      const articles = await scrapeArticle(topic);
+  
+      
+      await Article.insertMany(articles);
+  
+      res.json({ message: 'Scraping successful', articles });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  const getArticles=async (req, res) => {
+    try {
+        const articles = await Article.find().limit(5).sort({ _id: -1 });
+      res.json(articles);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch articles' });
+    }
+  }
+  module.exports ={createScrapArticle,getArticles}
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,33 +1,10 @@
-const Article = require("../models/articleModel");
+
 const express=require("express");
-const { scrapeArticle } = require("../config/scapper");
+const { createScrapArticle, getArticles } = require("../controllers/articleScrapperController");
+
 const scrapperRouter= express.Router()
-scrapperRouter.post('/scrape', async (req, res) => {
-    const { topic } = req.query;
+scrapperRouter.post('/scrape',createScrapArticle);
   
-    if (!topic) {
-      return res.status(400).json({ error: 'Topic is required' });
-    }
-  
-    try {
-      const articles = await scrapeArticle(topic);
-  
-      
-      await Article.insertMany(articles);
-  
-      res.json({ message: 'Scraping successful', articles });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  
-  scrapperRouter.get('/articles', async (req, res) => {
-    try {
-        const articles = await Article.find().limit(5).sort({ _id: -1 });
-      res.json(articles);
-    } catch (error) {
-      res.status(500).json({ error: 'Failed to fetch articles' });
-    }
-  });
+  scrapperRouter.get('/articles',getArticles );
   
   module.exports ={scrapperRouter}
